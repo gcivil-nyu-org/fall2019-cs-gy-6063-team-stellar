1fb866efb3f61239dc6aef0511379a1e27af09ea
Utkarsh Prakash <monsieurutkarsh@gmail.com>
2019-10-20 00:28:02 -0400

    unit test for sign-in and sign-up forms

--- user_account/forms.py
+++ user_account/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from captcha.fields import CaptchaField
 
 class UserSignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=100, help_text='Required')
+    email = forms.EmailField(max_length=100, help_text='Required(NYU Email ID)')
     
     def clean_email(self):
         data = self.cleaned_data['email']
@@ -13,18 +13,12 @@ class UserSignUpForm(UserCreationForm):
             raise forms.ValidationError("Please enter a NYU Email Address")
         return data
     
-    def save(self, commit=True):
-        user = super(UserSignUpForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
-        if commit:
-            user.save()
-        return user
-
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        
 class UserSignInForm(forms.Form):
-    username = forms.CharField(label="username", max_length=128,widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Username",'autofocus': ''}))
+    username = forms.CharField(label="username", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Username",'autofocus': ''}))
     password = forms.CharField(label="password", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder': "Password"}))
     captcha = CaptchaField(label='verifycode')
 
