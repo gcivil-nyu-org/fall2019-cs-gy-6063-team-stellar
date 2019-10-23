623056515cb75ef885ca613614350bd567261813
huangxinchi666 <huangxinchi666@gmail.com>
2019-10-23 16:18:42 -0400

    add travis

--- lunchNinja/settings.py
+++ lunchNinja/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k@d#ygg4htxk_illoz_68#9znhi(%k2l_^hadld236cdw%7&4^'
+SECRET_KEY = "k@d#ygg4htxk_illoz_68#9znhi(%k2l_^hadld236cdw%7&4^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,54 +33,54 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'user_account.apps.UserAccountConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'captcha',
+    "user_account.apps.UserAccountConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "captcha",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'lunchNinja.urls'
+ROOT_URLCONF = "lunchNinja.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
         },
-    },
+    }
 ]
 
-WSGI_APPLICATION = 'lunchNinja.wsgi.application'
+WSGI_APPLICATION = "lunchNinja.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -90,26 +90,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -121,13 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
-#SMTP Settings
+# SMTP Settings
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.mailtrap.io'
-EMAIL_HOST_USER = 'c3ad93d6737c1e'
-EMAIL_HOST_PASSWORD = 'f5a014dcafbc59'
-EMAIL_PORT = '2525'
+EMAIL_HOST = "smtp.mailtrap.io"
+EMAIL_HOST_USER = "c3ad93d6737c1e"
+EMAIL_HOST_PASSWORD = "f5a014dcafbc59"
+EMAIL_PORT = "2525"
 django_heroku.settings(locals(), test_runner=False)
 # django_heroku.settings(locals())
--- user_account/forms.py
+++ user_account/forms.py
@@ -3,22 +3,35 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from captcha.fields import CaptchaField
 
+
 class UserSignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=100, help_text='Required(NYU Email ID)')
-    
+    email = forms.EmailField(max_length=100, help_text="Required(NYU Email ID)")
+
     def clean_email(self):
-        data = self.cleaned_data['email']
-        domain = data.split('@')[1]
+        data = self.cleaned_data["email"]
+        domain = data.split("@")[1]
         if domain != "nyu.edu":
             raise forms.ValidationError("Please enter a NYU Email Address")
         return data
-    
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
-        
-class UserSignInForm(forms.Form):
-    username = forms.CharField(label="username", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Username",'autofocus': ''}))
-    password = forms.CharField(label="password", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder': "Password"}))
-    captcha = CaptchaField(label='verifycode')
+        fields = ("username", "email", "password1", "password2")
 
+
+class UserSignInForm(forms.Form):
+    username = forms.CharField(
+        label="username",
+        max_length=128,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Username", "autofocus": ""}
+        ),
+    )
+    password = forms.CharField(
+        label="password",
+        max_length=256,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Password"}
+        ),
+    )
+    captcha = CaptchaField(label="verifycode")
