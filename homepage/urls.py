10fbb8d13c8806277427bfe00edfafd6bea04662
Utkarsh Prakash <monsieurutkarsh@gmail.com>
2019-10-25 15:36:37 -0400

    added bootstrap and jquery

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,6 +2,4 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('homepage/', views.index),
-]
+urlpatterns = [path("homepage/", views.index)]
