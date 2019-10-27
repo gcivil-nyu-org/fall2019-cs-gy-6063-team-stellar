521b44d500c2220836cb68398ba0412ca9b2558b
YixinHu0321 <yh3244@nyu.edu>
2019-10-27 14:13:01 -0400

    Not working homepage modal

--- homepage/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from homepage.models import UserRequest
-
-class UserRequestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserRequest
-        field = ['school', 'department']
