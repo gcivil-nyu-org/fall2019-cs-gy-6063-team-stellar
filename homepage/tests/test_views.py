0f4cb061149f959ee14c64de71c41d40765daa67
YixinHu0321 <yh3244@nyu.edu>
2019-10-30 05:12:31 -0400

    change test cases

--- homepage/tests/test_views.py
+++ homepage/tests/test_views.py
@@ -4,19 +4,20 @@ from django.test import TestCase
 class UserserviceViewTest(TestCase):
     def test_view_url_exists_at_desired_location(self):
         response = self.client.get("/homepage/")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
 
     def test_view_url_accessible_by_name(self):
         response = self.client.get("/homepage/")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
         self.assertTemplateUsed(response, "homepage.html")
 
     def test_call_view_success_correct_fields(self):
         requestObj = {
+            "user_id": "1",
             "service_type": "Weekly",
             "school": "Tandon School of Engineering",
             "department": "Electrical Engineering",
-            "cuisine": ["Indian", "Pizza"],
+            "cuisine": "[Indian, Pizza]",
         }
         response = self.client.post("/serviceRequest/", requestObj)
         self.assertRedirects(response, "/")
