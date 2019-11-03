c25abf4e716f8484ea0fd5c79bf9b3bf35f936c2
huangxinchi666 <huangxinchi666@gmail.com>
2019-11-03 15:33:10 -0500

    add a script to generate users and store them in to a csv. Also generated 100 users from tandon school

--- homepage/views.py
+++ homepage/views.py
@@ -32,7 +32,7 @@ def merge():
         school_department[schoolitem[0]] = []
     for departmentitem in department_list:
 
-        department = departmentitem[0]
+        department.append(departmentitem[0])
         school_department[id_school[str(departmentitem[1])]].append(departmentitem[0])
         department_school[departmentitem[0]] = [id_school[str(departmentitem[1])]]
 
