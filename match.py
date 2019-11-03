0410572f0672dc9b58329470a7364d212f15edfc
YixinHu0321 <yh3244@nyu.edu>
2019-11-03 16:46:22 -0500

    change style

--- match.py
+++ match.py
@@ -1,4 +1,6 @@
 import csv
+
+
 def load_cuisine(file):
     with open(file, "r") as in_f:
         cuisinelist = []
@@ -7,12 +9,18 @@ def load_cuisine(file):
             if not line:
                 break
             linelist = line.strip().split(",")
-            if not linelist[7].strip('\"') in cuisinelist:
-                cuisinelist.append(linelist[7].strip('\"'))
+            if not linelist[7].strip('"') in cuisinelist:
+                cuisinelist.append(linelist[7].strip('"'))
         cuisinelist = cuisinelist[1:]
         return cuisinelist
-cuisine_list=load_cuisine("datasource\DOHMH_New_York_City_Restaurant_Inspection_Results.csv")
-cuisine_table={}
+
+
+cuisine_list = load_cuisine(
+    "datasource\DOHMH_New_York_City_Restaurant_Inspection_Results.csv"  # noqa: W605
+)
+cuisine_table = {}
+
+
 def load_user(file):
     with open(file, "r") as in_f:
         userlist = []
@@ -20,13 +28,13 @@ def load_user(file):
         for linelist in f_csv:
             userlist.append(linelist)
         return userlist[1:]
-userlist=load_user("users.csv")
+
+
+userlist = load_user("users.csv")
 print(userlist)
 for cuisine in cuisine_list:
-    cuisine_table[cuisine]=[]
+    cuisine_table[cuisine] = []
 for user in userlist:
-    p_cuisine_str=user[3].strip('[').strip(']')
-    p_cuisine_list=p_cuisine_str.split(',')
+    p_cuisine_str = user[3].strip("[").strip("]")
+    p_cuisine_list = p_cuisine_str.split(",")
     print(p_cuisine_list)
-
-
