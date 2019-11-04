05e387c01f1a1564488b9b53366551a559394fbb
YixinHu0321 <yh3244@nyu.edu>
2019-11-04 17:00:40 -0500

    email with common cuisines

--- match.py
+++ match.py
@@ -11,14 +11,22 @@ from user_account.models import LunchNinjaUser  # noqa: E402
 
 
 # send_email() will trigger mailtrap to send out email to matched users
-def send_email(user1, user2):
+def send_email(user1, user2, cuisinelist):
+    cuisineline = ""
+    for i in range(len(cuisinelist)):
+        if i == len(cuisinelist) - 1:
+            cuisineline = cuisineline + cuisinelist[i].name;
+        else:
+            cuisineline = cuisineline + cuisinelist[i].name + ",";
     email_subject = "Lunch Confirmation"
     message = (
         "You got it! You will have a lunch with the user "
         + user2.username
         + "("
         + user2.email
-        + ")."
+        + ").\n"
+        + "We will provide some restaurant recommandations based on both of your preferred cuisine type:\n"
+        + cuisineline
     )
     to_email = user1.email
     email = EmailMessage(email_subject, message, to=[to_email])
@@ -28,15 +36,18 @@ def send_email(user1, user2):
 # initiate_email() takes in matching result and call send_email() by 1 email/5 second rate
 def initiate_email(match):
     for each in match:
-        print(each[0])
-        print(each[1])
-        user1 = LunchNinjaUser.objects.get(id=each[0])
-        user2 = LunchNinjaUser.objects.get(id=each[1])
-        time.sleep(5)
-        send_email(user1, user2)
-        time.sleep(5)
-        send_email(user2, user1)
+        req1 = each[0]
+        req2 = each[1]
 
+        user1 = LunchNinjaUser.objects.get(id=req1.user_id)
+        user2 = LunchNinjaUser.objects.get(id=req2.user_id)
+        cuisine_set1 = set(req1.cuisines.all())
+        cuisine_set2 = set(req2.cuisines.all())
+        cuisinelist = list(cuisine_set1 & cuisine_set2)
+        time.sleep(5)
+        send_email(user1, user2, cuisinelist)
+        time.sleep(5)
+        send_email(user2, user1, cuisinelist)
 
 # change string to list
 def str_to_list(string):
@@ -49,23 +60,6 @@ def str_to_list(string):
         else:
             out_list.append(item)
     return out_list
-
-
-# def create_cuisine_table(cuisine_list, reqlist):
-#     cuisine_table = {}
-#     for cuisine in cuisine_list:
-#         cuisine_table[cuisine] = []
-#     for req in reqlist:
-#         real_p_cuisine_list = req.cuisines.all()
-#         for c in real_p_cuisine_list:
-#             cuisine_table[c].append(req.user_id)
-#
-#     return cuisine_table
-#
-#
-# cuisine_list = Cuisine.objects.all()
-
-# cuisine_table = create_cuisine_table(cuisine_list, reqlist)
 
 
 def cuisine_filter(matchpool, available_set, req):
@@ -88,8 +82,8 @@ def cuisine_filter(matchpool, available_set, req):
 
 def match():
     match_result = []
-    unmached_user_list = []
-    matched_user_list = []
+    unmached_user_request = []
+    matched_user_request = []
     matchpool = set()
     reqlist = UserRequest.objects.all()
 
@@ -117,47 +111,15 @@ def match():
                 result.append(user_id)
                 result.append(match_user_id)
                 match_result.append(result)
-                matched_user_list.append(req)
-                matched_user_list.append(match_request)
+                request_result = []
+                request_result.append(req)
+                request_result.append(match_request)
+                matched_user_request.append(request_result)
             except Exception:
-                unmached_user_list.append(req)
+                unmached_user_request.append(req)
     print(match_result)
-    # initiate_email(match_result)
-    print(matched_user_list)
-    print(unmached_user_list)
-
-    # for req in reqlist:
-    #     user_id = req.user_id
-    #
-    #     if user_id in matchpool:
-    #         # remove selected user
-    #         matchpool.remove(user_id)
-    #
-    #         # find available users for this user(filter)
-    #         available_set = set()
-    #         available_set = cuisine_filter(matchpool, available_set, req)
-    #         # available_set = matched_user_filter(matchpool, available_set, user)
-    #
-    #         # pick a user from the available users
-    #         try:
-    #             # match_request = random.choice(list(available_set))
-    #             # match_user_id = match_request.user_id
-    #             match_user_id = random.choice(list(available_set))
-    #             match_user = UserRequest.objects.filter(user_id=match_user_id)
-    #             matchpool.remove(match_user_id)
-    #             # result = str(user_id) + "----" + str(match_user_id)
-    #             result = []
-    #             result.append(user_id)
-    #             result.append(match_user_id)
-    #             match_result.append(result)
-    #             matched_user_list.append(req)
-    #             matched_user_list.append(match_user)
-    #         except Exception:
-    #             unmached_user_list.append(req)
-    # print(match_result)
-    # initiate_email(match_result)
-    # print(matched_user_list)
-    # print(unmached_user_list)
-
+    print(matched_user_request)
+    initiate_email(matched_user_request)
+    print(unmached_user_request)
 
 match()
