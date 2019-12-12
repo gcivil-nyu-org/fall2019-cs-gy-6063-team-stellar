37c65ebe2211812e72eac0ec939286909bad57f8
Utkarsh Prakash <monsieurutkarsh@gmail.com>
2019-12-11 21:45:38 -0500

    updated time celery

--- lunchNinja/celery.py
+++ lunchNinja/celery.py
@@ -6,8 +6,6 @@ from celery.task.schedules import crontab
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lunchNinja.settings")
 
-
-
 app = Celery("lunchNinja")
 
 app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
@@ -21,28 +19,23 @@ app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.conf.timezone = "UTC"
 app.conf.enable_utc = True
-# app.conf.update(
-#     BROKER_URL=os.environ["REDIS_URL"], CELERY_RESULT_BACKEND=os.environ["REDIS_URL"]
-# )
-# amqp://pcsrozek:Mgc_X5...@crane.rmq.cloudamqp.com/pcsrozek
-
 
 # Schedule periodic tasks.
 app.conf.beat_schedule = {
     # Run matching algorithm
     "run_matching_algorithm": {
         "task": "homepage.tasks.run_matching_algorithm",
-        "schedule": crontab(minute="*/1"),
+        "schedule": crontab(minute=0, hour=19),
     },
+    # Send feedback forms 
     "send_match_feedback": {
         "task": "homepage.tasks.send_match_feedback",
-        "schedule": crontab(minute=5, hour=15),
+        "schedule": crontab(minute=0, hour=14),
     },
 }
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
-
 
 @app.task(bind=True)
 def debug_task(self):
